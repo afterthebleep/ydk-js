aea72effc7f212657ff09a09ed73a34396741dea
aishaortiz <developer.aishaortiz@gmail.com>
2019-10-02 09:02:22 -0700

    added submit btn | applied answers collection

--- javascript/app.js
+++ javascript/app.js
@@ -13,16 +13,27 @@ $('#start').on('click', function () {
     for (var i = 0; i < questions.length; i++) {
       $('#sub-container').append('<h3>' + questions[i].question + '</h3>');
       var correctAnswer = questions[i].correct_answer
-      var answersList = questions[i].incorrect_answersß
+      var answersList = questions[i].incorrect_answers
+      var allOptions = [...answersList, correctAnswer]
+      //combine answers grp under one name 
       answersList.push(correctAnswer)
 
       for (var j = 0; j < answersList.length; j++) {
         if (answersList[j] !== correctAnswer) {
-          $('#sub-container').append("<input type = 'radio' name='question' " + [i] + " 'value=' " + answersList[j] + "' > " + answersList[j]);
+          
+          $('#sub-container').append("<input type = 'radio' name='question" + [i] + "' value= " + answersList[j] + "' > " + answersList[j]);
         } else {
-          $('#sub-container').append("<input type = 'radio' name='correctAnswer' " + [i] + " 'value=' " + answersList[j] + "' > " + answersList[j]);
+          $('#sub-container').append("<input type = 'radio' name='question" + [i] + "' value= " + answersList[j] + "' data-answer = 'correctAnswer'> " + answersList[j]);
         }
       }
     }
+    $('#sub-container').append('<br>'+'<button id="submit"> Submit </button>');
+    $('#submit').on('click', function(e) {
+      e.preventDefault();
+      const userSelections = $('#sub-container').serializeArray();
+      console.log(questions);
+      console.log(userSelections);
+    });
   }) 
 });
+
